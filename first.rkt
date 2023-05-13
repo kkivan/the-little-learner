429b0a7cb911a2607f784ba3672c62d2339106bd
Ivan Kvyatkovskiy <ivan.kvyatkovski@gmail.com>
2023-05-13 13:52:16 +0700

    adds more

--- first.rkt
+++ first.rkt
@@ -12,13 +12,14 @@
 (define line
   (λ (x)
     (λ (theta)
-      (let ((w (car theta))
-        (b (car (cdr theta))))
+      (let ((w (ref theta 0))
+        (b (ref theta 1)))
       (+ (* w x) b)))))
 
 (define points '(1 2 3))
 
 ((line 1) (list 1 10))
+
 (map line points)
 
 (define line-xs
@@ -26,8 +27,6 @@
 
 (define line-ys
   (tensor 1.8 1.2 4.2 3.3))
-
-(define theta (list 1.0 0.0))
 
 
 ((line 7.3) (list 1.0 0.0))
@@ -53,7 +52,6 @@
 
 (gradient-of obj (list 0 0))
 
-
 (define revise
   (λ (f revs θ)
     (cond
@@ -67,10 +65,11 @@
            (- p 3))
          θ)))
 
-(revise f 5 (list 1 2 3))
+(revise f 5 (list 2 3))
 
-(define alpha 0.01)
-(define revs 1000)
+(declare-hyper alpha)
+(declare-hyper revs)
+(declare-hyper batch-size)
 
 (define gradient-descent
   (λ (obj θ)
@@ -81,12 +80,112 @@
                     (∇ obj Θ)))))
       (revise f revs θ))))
 
-(gradient-descent obj (list 0.0 0.0))
+(with-hypers ((alpha 0.01)
+              (revs 1000))
+  (gradient-descent obj (list 0.0 0.0)))
 
 
-(declare-hyper smaller)
-(declare-hyper larger)
-smaller
+(define quad
+  (λ (t)
+    (λ (θ)
+      (+ (* (tref θ 0) (sqr t))
+         (+ (* (tref θ 1) t) (tref θ 2))))))
 
-(with-hypers ((smaller 1 ) (larger 2000))
-  (+ smaller larger))
+(define plane
+  (λ (t)
+    (λ (θ)
+      (+ (• (ref θ 0) t) (ref θ 1)))))
+
+(define •
+  (λ (w t)
+    (sum (* w t))))
+
+(define plane-xs
+  (tensor (tensor 1.0 2.05)
+          (tensor 1.0 3.0)
+          (tensor 2.0 2.0)
+          (tensor 2.0 3.91)
+          (tensor 3.0 6.13)
+          (tensor 4.0 8.09)))
+
+(define plane-ys
+  (tensor 13.99
+          15.99
+          18.0
+          22.4
+          30.2
+          37.94))
+
+(time (with-hypers ((revs 1000)
+              (alpha 0.001))
+  (gradient-descent
+   ((l2-loss plane) plane-xs plane-ys)
+   (list (tensor 0.0 0.0) 5.78))))
+
+((plane
+  (tensor 2.0 3.91))
+ (list (tensor 3.5824044661594274
+                 2.1381545838037486)
+         6.371957766427741))
+
+(samples 20 3)
+
+
+(define sampling-obj
+  (λ (expectant xs ys)
+    (let ((n (tlen xs)))
+      (λ (θ)
+        (let ((b (samples n batch-size)))
+          ((expectant (trefs xs b) (trefs ys b)) θ))))))
+
+(time (with-hypers
+    ((revs 1000)
+     (alpha 0.01)
+     (batch-size 4))
+  (gradient-descent
+   (sampling-obj
+    (l2-loss line) line-xs line-ys)
+   (list 0.0 0.0))))
+
+(time (with-hypers ((revs 15000)
+              (alpha 0.001))
+  (gradient-descent
+   ((l2-loss plane) plane-xs plane-ys)
+   (list (tensor 0.0 0.0) 0.0))))
+
+(time
+ (with-hypers
+     ((revs 15000)
+      (alpha 0.001)
+      (batch-size 4))
+   (gradient-descent
+    (sampling-obj
+     (l2-loss plane) plane-xs plane-ys)
+    (list (tensor 0.0 0.0) 0.0))))
+
+      
+(define lonely-i
+  (λ (θ)
+    (map (λ (p)
+           (list p))
+         θ)))
+
+(define lonely-d
+  (λ (Θ)
+    (map (λ (p)
+           (ref p 0))
+         Θ)))
+
+(define lonely-u
+  (λ (Θ gs)
+    (map (λ (p g)
+           (list (- (tref p 0) (* alpha g))))
+         Θ
+         gs)))
+
+
+(lonely-d (lonely-i (list 1 2 3)))
+
+
+         
+            
